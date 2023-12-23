6fb0d4b9086c6ba84a940e9c649c2bb1441f4142
Javier <154339364+codeMancer1@users.noreply.github.com>
2023-12-23 09:23:39 -0500

    initialcommit

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-function efectoHabilidades() {
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if (distancia_skills >= 300) {
-        let habilidades = document.getElementsByClassName("barra-skill");
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
         habilidades[0].classList.add("javascript");
         habilidades[1].classList.add("htmlcss");
         habilidades[2].classList.add("photoshop");
@@ -14,10 +32,12 @@ function efectoHabilidades() {
         habilidades[8].classList.add("dedicacion");
         habilidades[9].classList.add("proyect");
         habilidades[10].classList.add("linux");
+      
     }
 }
 
-// Detecto el scrolling para aplicar la animación de la barra de habilidades
-window.onscroll = function () {
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
     efectoHabilidades();
-}
+} 
