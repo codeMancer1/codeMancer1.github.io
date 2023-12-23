c697a8133b8e548a78d0028474b2d47c5875a1f6
Javier <154339364+codeMancer1@users.noreply.github.com>
2023-12-23 09:14:17 -0500

    initial commit

--- script.js
+++ script.js
@@ -23,7 +23,6 @@ function efectoHabilidades(){
         let habilidades = document.getElementsByClassName("progreso");
         habilidades[0].classList.add("javascript");
         habilidades[1].classList.add("htmlcss");
-        habilidades[9].classList.add("linux");
         habilidades[2].classList.add("photoshop");
         habilidades[3].classList.add("wordpress");
         habilidades[4].classList.add("drupal");
